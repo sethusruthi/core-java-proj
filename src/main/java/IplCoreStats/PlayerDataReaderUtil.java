4390e1a98d0191e322c37028d6505b0819733505
Y. Sethu Sruthi <y.sethusruthi@gmail.com>
2024-03-18 21:50:54 -0400

    added Assignment questions

---
+++ src/main/java/IplCoreStats/PlayerDataReaderUtil.java
@@ -0,0 +1,22 @@
+package IplCoreStats;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
+
+public final class PlayerDataReaderUtil {
+    private PlayerDataReaderUtil() {
+    }
+
+    public static List<Player> loadPlayersData() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.readValue(
+                    PlayerDataReaderUtil.class.getResource("/players_data.json"),
+                    new TypeReference<List<Player>>() {});
+        } catch (Exception e) {
+            throw new RuntimeException("Error while loading player data " + e);
+        }
+    }
+}
